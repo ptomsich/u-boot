f9fb49273d7606b5e821d30d299d251597775644
Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
2017-02-15 22:13:37 +0100

    sunxi: add pinctrl (UCLASS_PINCTRL) support for sunxi and tie back into GPIO
    This change adds a full device-model pinctrl driver for sunxi (tested with
    sun50iw1p1) based on the support available in Linux.
    
    Details are:
     * implements a driver for pinctrl devices and assigns sun50i-a64-pinctrl
       and sun50i-a64-r-pinctrl to it
     * defines and implements a binding for sunxi-style GPIO banks (to make it
       easier to describe controllers like the A64 which may not start at 'A')
       and provide the necessary translation mechanisms:
       - As all our gpio-reference point back to either <&pio ..> or <&r_pio ..>
         the device framework will try to access a UCLASS_GPIO device bound to
         the same node id as the pinctrl device to perform it's of_xlate lookup.
         For this, we provide a 'gpiobridge' driver (which needs to access the
         platdata of the pinctrl device) and which can then map accesses to an
         actual GPIO bank device.
       - For the individual GPIO banks, we use a new driver (which shares most
         of its ops with the existing sunxi_gpio driver, except probe and bind)
         and provides configuration without any platdata structure.
     * lifts and reuses the pinctrl-sunxi.h and pinctrl-sun50i-a64.c files from
       Linux (thanks to Maxime and Andre) and adds a pinctrl-sun50i-a64-r.c (to
       be picked up for inclusion into Linux again)
    
    The active DM tree at runtime (with this enabled) should look similar to
    the following:
    
         pinctrl     [ + ]    |   |-- pinctrl@1c20800
         gpio        [ + ]    |   |   |-- gpiob@24
         gpio        [ + ]    |   |   |-- gpioc@48
         gpio        [ + ]    |   |   |-- gpiod@6c
         gpio        [ + ]    |   |   |-- gpioe@90
         gpio        [ + ]    |   |   |-- gpiof@b4
         gpio        [ + ]    |   |   |-- gpiog@d8
         gpio        [ + ]    |   |   |-- gpioh@fc
         pinconfig   [ + ]    |   |   |-- uart0_pins_a
         pinconfig   [   ]    |   |   |-- uart0_pins_b
         pinconfig   [   ]    |   |   |-- uart1_2pins
         pinconfig   [   ]    |   |   |-- uart1_4pins
         pinconfig   [   ]    |   |   |-- uart2_2pins
         pinconfig   [   ]    |   |   |-- uart2_4
         pinconfig   [   ]    |   |   |-- uart3
         pinconfig   [   ]    |   |   |-- uart3_2
         pinconfig   [   ]    |   |   |-- uart3_4
         pinconfig   [   ]    |   |   |-- uart4_2
         pinconfig   [   ]    |   |   |-- uart4_4
         pinconfig   [ + ]    |   |   |-- mmc0
         pinconfig   [ + ]    |   |   |-- mmc0_cd_pin
         pinconfig   [   ]    |   |   |-- mmc1
         pinconfig   [   ]    |   |   |-- mmc2
         pinconfig   [ + ]    |   |   |-- mmc2_8bit
         pinconfig   [   ]    |   |   |-- i2c0_pins
         pinconfig   [   ]    |   |   |-- i2c1_pins
         pinconfig   [   ]    |   |   |-- i2c2_pins
         pinconfig   [   ]    |   |   |-- rmii_pins
         pinconfig   [ + ]    |   |   |-- rgmii_pins
         pinconfig   [ + ]    |   |   |-- spi0_pins
         pinconfig   [   ]    |   |   |-- spi1_pins
         pinconfig   [ + ]    |   |   |-- led_pins_sdio
         pinconfig   [ + ]    |   |   |-- ethphy_reset_pin
         gpio        [ + ]    |   |   `-- gpiobridge
         pinctrl     [ + ]    |   |-- pinctrl@01f02c00
         gpio        [ + ]    |   |   |-- gpiol@0
         pinconfig   [ + ]    |   |   |-- led_pins_power
         gpio        [ + ]    |   |   `-- gpiobridge
    
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

---
+++ drivers/pinctrl/sunxi/pinctrl-sun50i-a64-r.c
@@ -0,0 +1,92 @@
+/*
+ * Allwinner A64 SoCs pinctrl driver.
+ *
+ * Copyright (C) 2017 Theobroma Systems Design und Consulting GmbH.
+ *
+ * Based on pinctrl-sun7i-a20.c, which is:
+ * Copyright (C) 2014 Maxime Ripard <maxime.ripard@free-electrons.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#include <common.h>
+
+#include "pinctrl-sunxi.h"
+
+static const struct sunxi_desc_pin a64_r_pins[] = {
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 0),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_rsb"),		/* SCK */
+		  SUNXI_FUNCTION(0x3, "s_i2c"),		/* SCK */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 0)),	/* EINT0 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 1),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_rsb"),		/* SDA */
+		  SUNXI_FUNCTION(0x3, "s_i2c"),		/* SDA */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 1)),	/* EINT1 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 2),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_uart"),	/* TX */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 2)),	/* EINT2 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 3),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_uart"),	/* RX */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 3)),	/* EINT3 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 4),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_jtag"),        /* MS */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 4)),	/* EINT4 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 5),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_jtag"),        /* CK */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 5)),	/* EINT5 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 6),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_jtag"),        /* DO */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 6)),	/* EINT6 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 7),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_jtag"),        /* DI */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 7)),	/* EINT7 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 8),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_i2c"),         /* SCK */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 8)),	/* EINT8 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 9),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_i2c"),         /* SDA */
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 9)),	/* EINT9 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 10),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_pwm"),
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 10)),	/* EINT10 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 11),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION(0x2, "s_cir"),
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 11)),	/* EINT11 */
+	SUNXI_PIN(SUNXI_PINCTRL_PIN(L, 12),
+		  SUNXI_FUNCTION(0x0, "gpio_in"),
+		  SUNXI_FUNCTION(0x1, "gpio_out"),
+		  SUNXI_FUNCTION_IRQ_BANK(0x6, 0, 12)),	/* EINT12 */
+};
+
+const struct sunxi_pinctrl_desc a64_r_pinctrl_data = {
+	.pins = a64_r_pins,
+	.npins = ARRAY_SIZE(a64_r_pins),
+	.pin_base = PL_BASE,
+	.irq_banks = 1,
+};
